17a547d3672558a7f36a05722728c8688b3f4eaa
Ben <blearn5241@gmail.com>
2022-08-20 23:12:32 -0400

    small data model restructure, owd updates, and sharing rule

--- scratchOrgSetup.sh
+++ scratchOrgSetup.sh
@@ -8,15 +8,18 @@ do
 done
 
 username=${username:-$(sfdx force:config:get defaultusername --json | jq -r '.result[0].value')}
-package=${package:?"Please input the package to install."}
+package=${package:?"Please input the package to install the dependencies of."}
 
 jsonDependencies=($(sfdx force:data:soql:query -u RecommendedBadges -t -q "SELECT Dependencies FROM SubscriberPackageVersion WHERE Id='$package'" --json | jq -c '[.result.records[0].Dependencies.ids | .[].subscriberPackageVersionId]'))
 dependencies=()
 dependencies+=( $(echo ${jsonDependencies:1:(${#jsonDependencies}-2)} | tr "," " " | tr -d '"') )
 
-for id in ${dependencies[@]}
-do
-    echo "Installing dependent package: "$id
-    sfdx force:package:install -p $id -u $username -r -w 20
-    echo ""
-done
+if ((${#dependencies[@]} > 0))
+then
+    for id in ${dependencies[@]}
+    do
+        echo "Installing dependent package: "$id
+        sfdx force:package:install -p $id -u $username -r -w 20
+        echo ""
+    done
+fi
