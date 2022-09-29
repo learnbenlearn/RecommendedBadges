bf498425fe5ca68c4e7dd93f0831e6dd68cd68e9
Ben <blearn5241@gmail.com>
2022-09-28 20:04:31 -0400

    working on node script to get limits

---
+++ getLimits.js
@@ -0,0 +1,14 @@
+#!/bin/env node
+
+const exec = util.promisify(require('child_process').exec);
+
+async function getLimits() {
+    try {
+        const {stdout, stderr} = await exec('sfdx force:limits:api:display --json');
+        console.log(process.env.HUB_USER_NAME)
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+getLimits();
