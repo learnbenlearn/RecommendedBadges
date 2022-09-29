495e284807b155db2dc32e07cd4daf68f5352725
Ben <blearn5241@gmail.com>
2022-09-28 20:11:34 -0400

    comment out command

--- getLimits.js
+++ getLimits.js
@@ -5,7 +5,7 @@ const exec = util.promisify(require('child_process').exec);
 
 async function getLimits() {
     try {
-        const {stdout, stderr} = await exec('sfdx force:limits:api:display --json');
+        //const {stdout, stderr} = await exec('sfdx force:limits:api:display --json');
         console.log(process.env.HUB_USER_NAME)
     } catch(err) {
         console.error(err);
