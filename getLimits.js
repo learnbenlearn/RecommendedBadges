3272726856ba342e7217ce5daf12be9b549d894a
Ben <blearn5241@gmail.com>
2022-09-28 20:34:36 -0400

    finish limit parsing

--- getLimits.js
+++ getLimits.js
@@ -3,6 +3,9 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
+const SCRATCH_ORG_LIMIT = 'DailyScratchOrgs';
+const PACKAGE_VERSION_LIMIT = 'Package2VersionCreates';
+
 async function getLimits() {
     try {
         const {stdout, stderr} = await exec(`sfdx force:limits:api:display -u ${process.env.HUB_ALIAS} --json`);
@@ -11,7 +14,20 @@ async function getLimits() {
         } else {
             let jsonResponse = JSON.parse(stdout);
             let limitList = jsonResponse.result;
-            console.log(limitList);
+            let remainingScratchOrgs;
+            let remainingPackageVersions;
+
+            for(let limit of limitList) {
+                switch(limit.name) {
+                    case SCRATCH_ORG_LIMIT:
+                        remainingScratchOrgs = limit.remaining;
+                        break;
+                    case PACKAGE_VERSION_LIMIT:
+                        remainingPackageVersions = limit.remaining;
+                }
+            }
+
+            return `${remainingScratchOrgs} ${remainingPackageVersions}`
         }
     } catch(err) {
         console.error(err);
