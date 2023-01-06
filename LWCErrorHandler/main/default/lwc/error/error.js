cf6c1a1676e09ca033defb53f2181680df31630c
Ben <blearn5241@gmail.com>
2023-01-06 08:26:04 -0500

    work on exception handling, removed test-data from repo

---
+++ LWCErrorHandler/main/default/lwc/error/error.js
@@ -0,0 +1,79 @@
+import { LightningElement, api, wire } from 'lwc';
+
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { getRecord } from 'lightning/uiRecordApi';
+
+import PROFILE_ID_FIELD from '@salesforce/schema/User.ProfileId';
+import PROFILE_NAME_FIELD from '@salesforce/schema/User.Profile.Name';
+
+import USER_ID from '@salesforce/user/Id';
+
+import ErrorModal from 'c/errorModal';
+
+const ERROR_VARIANT = 'error';
+const ERROR_MODAL_DESCRIPTION = 'Displays error message';
+const ERROR_MODAL_LABEL = 'Error Modal';
+const ERROR_MODAL_SIZE = 'small';
+const LOGGER_NAME = 'c-logger';
+const SAVE_METHOD = 'EVENT_BUS';
+const SYSTEM_ADMINISTRATOR_PROFILE = 'System Administrator';
+
+export default class Error extends LightningElement {
+    @api admProfiles = [SYSTEM_ADMINISTRATOR_PROFILE];
+    @api displayModal;
+    @api errorModalSize = ERROR_MODAL_SIZE;
+    @api saveMethod = SAVE_METHOD;
+    error;
+    sysAdminView;
+
+    @wire(getRecord, {recordId: `${USER_ID}`, fields: [PROFILE_ID_FIELD, PROFILE_NAME_FIELD]})
+    parseRecord({error, data}) {
+        if(error) {
+            handleError(error);
+        } else if(data) {
+            if(this.admProfiles.indexOf(data.fields.Profile.displayValue) != -1) {
+                this.sysAdminView = true;
+            }
+        }
+    }
+
+    @api async handleError(error, displayModal) {
+        this.logError(error);
+        if(this.sysAdminView || displayModal) {
+            this.openModal(error);
+        } else {
+            this.displayErrorToast(error);
+        }
+    }
+
+    displayErrorToast(error) {
+        this.dispatchEvent(new ShowToastEvent({
+            variant: ERROR_VARIANT,
+            message: error.body.message,
+            title: error.body.exceptionType
+        }));
+    }
+
+    logError(error) {
+        const logger = this.template.querySelector(LOGGER_NAME);
+        logger.error(error.message).setError(error);
+        logger.saveLog(this.saveMethod);
+    }
+
+    async openModal(error) {
+        this.error = {
+            exceptionType: error.body.exceptionType,
+            message: error.body.message
+        };
+        if(this.sysAdminView) {
+            this.error.stackTrace = error.body.stackTrace;
+        }
+        await ErrorModal.open({
+            label: ERROR_MODAL_LABEL,
+            description: ERROR_MODAL_DESCRIPTION,
+            size: this.errorModalSize,
+            error: this.error,
+            sysAdminView: this.sysAdminView
+        });
+    }
+}
---
+++ LWCErrorHandler/main/default/lwc/errorModal/errorModal.js
@@ -0,0 +1,11 @@
+import { api } from 'lwc';
+import LightningModal from 'lightning/modal';
+
+export default class ErrorModal extends LightningModal {
+    @api error;
+    @api sysAdminView;
+
+    handleOkay() {
+        this.close('okay');
+    }
+}
