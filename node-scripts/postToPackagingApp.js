81c10126fa00aadf599ff6d0364f5d6257232dde
Ben <blearn5241@gmail.com>
2023-01-15 21:52:56 -0500

    cleaned package versions listed in sfdx-project.json, corrected package dependency diagram

--- node-scripts/postToPackagingApp.js
+++ node-scripts/postToPackagingApp.js
@@ -1,15 +1,20 @@
 const axios = require('axios');
+const fs = require('fs');
 
 const HEROKU_ENDPOINT = 'https://recommended-badges-warehouse.herokuapp.com/job';
+const SORTED_PACKAGES_TO_UPDATE_FILE = '/tmp/artifacts/packagesToUpdate.txt';
 const PULL_REQUEST_URL = process.env.CIRCLE_PULL_REQUEST;
 
 async function postToPackagingApp() {
     try {
         let pullRequestNumber = PULL_REQUEST_URL.substring(PULL_REQUEST_URL.lastIndexOf('/') + 1);
+        let sortedPackagesToUpdate = fs.readFileSync(SORTED_PACKAGES_TO_UPDATE_FILE).split('\n');
         const res = await axios.post(
             HEROKU_ENDPOINT,
             {
                 pullRequestNumber,
+                jobNumber: process.env.CIRCLE_BUILD_NUM,
+                sortedPackagesToUpdate
             },
             {
                 headers: {
