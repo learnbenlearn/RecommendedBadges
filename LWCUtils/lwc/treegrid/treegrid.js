5adcb9003ded7d4d85ec87dc71bdf6dd1e780f4e
Ben <blearn5241@gmail.com>
2024-06-05 20:21:00 -0400

    removed poc code from treegrid

--- LWCUtils/lwc/treegrid/treegrid.js
+++ LWCUtils/lwc/treegrid/treegrid.js
@@ -13,14 +13,4 @@ export default class Treegrid extends LightningElement {
     @api collapseAll() {
         this.template.querySelector('lightning-tree-grid').collapseAll();
     }
-
-    /*
-        .slds-table thead th{
-            background-color:var(--slds-g-color-neutral-base-95, #f3f3f3);
-            color:var(--slds-g-color-neutral-base-30, #444444);
-            padding:0.25rem 0.5rem;
-            font-weight:700;
-            line-height:normal;
-        }
-    */
 }
