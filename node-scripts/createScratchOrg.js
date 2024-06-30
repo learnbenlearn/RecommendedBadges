dfff308a5887a4446954a1a526db40adde7bf6aa
Ben <blearn5241@gmail.com>
2024-06-30 19:26:51 -0400

    debugging

--- node-scripts/createScratchOrg.js
+++ node-scripts/createScratchOrg.js
@@ -11,11 +11,11 @@ async function createScratchOrg() {
     
     process.stdout.write(`stdout ${stdout}`);
     if(stderr) {
-        process.stderr.write(`Error in createScratchOrg(): ${stderr}`);
+        //process.stderr.write(`Error in createScratchOrg(): ${stderr}`);
         fs.writeFileSync('/tmp/artifacts/createScratchOrgError.json', stderr)
         process.exit(1);
     } else if(stdout) {
-        process.stdout.write(`Created scratch org: ${stdout}`);
+        //process.stdout.write(`Created scratch org: ${stdout}`);
     }
 }
 
