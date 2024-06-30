c0fcd0dc3c8464bf1533bf174be212af085a89f3
Ben <blearn5241@gmail.com>
2024-06-30 19:40:01 -0400

    fixing undefined error

--- node-scripts/createScratchOrg.js
+++ node-scripts/createScratchOrg.js
@@ -4,18 +4,19 @@ const exec = util.promisify(require('child_process').exec);
 
 async function createScratchOrg() {
     try {
+        process.stdout.write(`stdout ${stdout}`);
         ({stdout, stderr} = await exec(`sf org create scratch -f ${process.env.SCRATCH_ORG_DEFINITION_FILE} -v ${process.env.HUB_ALIAS} -d -a ${process.env.TEST_ORG_ALIAS} --no-track-source -w ${process.env.WAIT_TIME} -y 1`));
+
+    
+        if(stderr) {
+            //process.stderr.write(`Error in createScratchOrg(): ${stderr}`);
+            fs.writeFileSync('/tmp/artifacts/createScratchOrgError.json', stderr)
+            process.exit(1);
+        } else if(stdout) {
+            //process.stdout.write(`Created scratch org: ${stdout}`);
+        }
     } catch(e) {
         process.stdout.write(`error ${e}`);
-    }
-    
-    process.stdout.write(`stdout ${stdout}`);
-    if(stderr) {
-        //process.stderr.write(`Error in createScratchOrg(): ${stderr}`);
-        fs.writeFileSync('/tmp/artifacts/createScratchOrgError.json', stderr)
-        process.exit(1);
-    } else if(stdout) {
-        //process.stdout.write(`Created scratch org: ${stdout}`);
     }
 }
 
