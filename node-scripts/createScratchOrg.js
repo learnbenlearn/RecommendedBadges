816af3eb9ddbd44168081fa6b7c65f9f0ea448ad
Ben <blearn5241@gmail.com>
2024-06-30 18:12:21 -0400

    writing stderr on scratch org create to file for debugging

--- node-scripts/createScratchOrg.js
+++ node-scripts/createScratchOrg.js
@@ -5,10 +5,10 @@ const exec = util.promisify(require('child_process').exec);
 async function createScratchOrg() {
     ({stdout, stderr} = await exec(`sf org create scratch -f ${process.env.SCRATCH_ORG_DEFINITION_FILE} -v ${process.env.HUB_ALIAS} -d -a ${process.env.TEST_ORG_ALIAS} --no-track-source -w ${process.env.WAIT_TIME} -y 1`));
     
-    process.stdout.write(`stderr ${stderr}`);
     process.stdout.write(`stdout ${stdout}`);
     if(stderr) {
         process.stderr.write(`Error in createScratchOrg(): ${stderr}`);
+        fs.writeFileSync('/tmp/artifacts/createScratchOrgError.json', stderr)
         process.exit(1);
     } else if(stdout) {
         process.stdout.write(`Created scratch org: ${stdout}`);
