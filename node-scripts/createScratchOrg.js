fd449f1716294ddcd1c7de5697a971db2b7bc703
Ben <blearn5241@gmail.com>
2024-07-01 15:17:20 -0400

    reordering stdout/stderr checks so build doesn't fail just bc sf cli writes ongoing output to stderr

--- node-scripts/createScratchOrg.js
+++ node-scripts/createScratchOrg.js
@@ -6,14 +6,12 @@ async function createScratchOrg() {
     try {
         ({stdout, stderr} = await exec(`sf org create scratch -f ${process.env.SCRATCH_ORG_DEFINITION_FILE} -v ${process.env.HUB_ALIAS} -d -a ${process.env.TEST_ORG_ALIAS} --no-track-source -w ${process.env.WAIT_TIME} -y 1`));
 
-        process.stdout.write(`stdout ${stdout}`);
-    
-        if(stderr) {
+        if(stdout) {
+            process.stdout.write(`Created scratch org: ${stdout}`);
+        } else if(stderr) {
             process.stderr.write(`Error in createScratchOrg(): ${stderr}`);
             fs.writeFileSync('/tmp/artifacts/createScratchOrgError.json', stderr)
             process.exit(1);
-        } else if(stdout) {
-            process.stdout.write(`Created scratch org: ${stdout}`);
         }
     } catch(e) {
         process.stdout.write(`error ${e}`);
