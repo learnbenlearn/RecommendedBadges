15012ab23fefc3840c2380037b9b1e92420d3dbe
Ben <blearn5241@gmail.com>
2024-06-30 20:09:25 -0400

    fixed write of stdout

--- node-scripts/createScratchOrg.js
+++ node-scripts/createScratchOrg.js
@@ -4,9 +4,9 @@ const exec = util.promisify(require('child_process').exec);
 
 async function createScratchOrg() {
     try {
-        process.stdout.write(`stdout ${stdout}`);
         ({stdout, stderr} = await exec(`sf org create scratch -f ${process.env.SCRATCH_ORG_DEFINITION_FILE} -v ${process.env.HUB_ALIAS} -d -a ${process.env.TEST_ORG_ALIAS} --no-track-source -w ${process.env.WAIT_TIME} -y 1`));
 
+        process.stdout.write(`stdout ${stdout}`);
     
         if(stderr) {
             //process.stderr.write(`Error in createScratchOrg(): ${stderr}`);
